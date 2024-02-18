7634abc34f349cb4888aa806d580976694bb0b99
AntonTenS <142214331+AntonTenS@users.noreply.github.com>
2024-02-18 09:52:14 +0300

    change in App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import Hello from "./hello";
 import React from "react";
 
-class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1>
-          Hello {name}
-          <Hello></Hello>
-        </h1>
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <h1>
+        Hello {name}
+        <Hello></Hello>
+      </h1>
+    </>
+  );
 }
 
 export default App;
