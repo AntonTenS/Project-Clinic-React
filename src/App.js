8bd5b666e35eacce6f085ad3a95b5ced90e5b2ab
AntonTenS <142214331+AntonTenS@users.noreply.github.com>
2024-02-18 14:51:57 +0300

    add useContext

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import Hello from "./hello";
+import Hello from "./hello.jsx";
 import React from "react";
 import { createContext, useState } from "react";
+import { AuthContext } from "./context";
 
 function App() {
+  const [user, setUser] = useState("Jesse Hall");
+  let redman = {
+    addres: "ростов",
+    home: 10,
+  };
   return (
     <>
-      <h1>Hello</h1>
-      <Hello></Hello>
+      <AuthContext.Provider value={redman}>
+        <h1>{`hello ${user}`}</h1>
+        <Hello>{`hello`}</Hello>
+      </AuthContext.Provider>
     </>
   );
 }
