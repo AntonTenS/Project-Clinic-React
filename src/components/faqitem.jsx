63692ae4314957d03895b9537701eccc8dc230df
AntonTenS <142214331+AntonTenS@users.noreply.github.com>
2024-02-29 13:53:23 +0300

    add strong line for answer

--- src/components/faqitem.jsx
+++ src/components/faqitem.jsx
@@ -9,6 +9,25 @@ export function Faqitem({ object, index }) {
   if (!get) {
     return <div>Загрузка текста...</div>;
   }
+
+  const formatAnswer = (answer, index) => {
+    const sentences = answer.split(". ");
+    if (index === 0) {
+      return (
+        <p key={index} className={toggle ? style.answer : style.noanswer}>
+          {sentences.join(". ")}
+        </p>
+      );
+    } else {
+      const firstSentence = <strong>{sentences[0]}</strong>;
+      const restSentences = sentences.slice(1).join(". ");
+      return (
+        <p key={index} className={toggle ? style.answer : style.noanswer}>
+          {firstSentence}. {restSentences}
+        </p>
+      );
+    }
+  };
   const [toggle, settoggle] = useState(false);
 
   const Swith = () => {
@@ -19,11 +38,7 @@ export function Faqitem({ object, index }) {
       <div className={style.block} key={index} onClick={Swith}>
         <div className={style.left}>
           <p className={style.question}>{get.faq.faqs[object].question}</p>
-          {Object.keys(get.faq.faqs[object].answer).map((key, index) => (
-            <p key={index} className={toggle ? style.answer : style.noanswer}>
-              {get.faq.faqs[object].answer[key]}
-            </p>
-          ))}
+          {Object.keys(get.faq.faqs[object].answer).map((key, index) => formatAnswer(get.faq.faqs[object].answer[key], index))}
         </div>
         <div className={style.right}>
           <div className={style.plus}>
